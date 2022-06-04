ff7c619b11b36929448931cff017c812925a6f76
arcetros <75669794+arcetros@users.noreply.github.com>
2022-06-04 12:00:16 +0800

    make CurrentForecast component work

--- src/components/CurrentForecast.tsx
+++ src/components/CurrentForecast.tsx
@@ -1,16 +1,18 @@
 import { MdOutlineOpenInNew } from 'react-icons/md';
 import moment from 'moment';
+import momenttz from 'moment-timezone';
 import useMobile from '../hooks/useMobile';
 import { useSelector } from '../store';
 import RainChart from './RainChart';
+import { Date, Temperature, Location } from './UI/Skeleton/CurrentSkeleton';
 
 export default function CurrentForecast() {
   const currentWeather = useSelector((state) => state.weather?.current_weather);
+  const currentForecast = useSelector((state) => state.weather?.forecasts);
   const dailyForecast = useSelector((state) => state.weather?.forecasts?.daily[0]);
 
   const isMobile = useMobile();
-
-  console.log(dailyForecast);
+  const currentTime = momenttz.tz(currentForecast?.timezone).format('LT');
 
   return (
     <div className="order-first lg:order-last relative lg:sticky right-0 h-auto w-full lg:h-screen lg:w-96 bg-primary overflow-y-hidden lg:overflow-y-auto">
@@ -34,34 +36,55 @@ export default function CurrentForecast() {
           </div>
           <div className="w-full">
             <div className="flex flex-col gap-y-4">
-              <div className="flex justify-center items-center gap-x-4">
-                <img
-                  src={`http://openweathermap.org/img/wn/${currentWeather?.weather[0].icon}${
-                    isMobile ? '' : '@2x'
-                  }.png`}
-                  alt="Weather Icon"
-                  className="relative"
-                />
-                <div className="flex flex-col gap-y-1 text-white">
-                  <span className="text-xl tracking-wide ">Today</span>
-                  <span className="text-gray-300 text-xs">{moment().format('LL')}</span>
-                </div>
+              <div className="flex items-center justify-center gap-x-4">
+                {currentWeather ? (
+                  <>
+                    <img
+                      src={`http://openweathermap.org/img/wn/${currentWeather.weather[0].icon}${
+                        isMobile ? '' : '@2x'
+                      }.png`}
+                      alt="Weather Icon"
+                      className="relative"
+                    />
+                    <div className="flex flex-col gap-y-1 text-white">
+                      <span className="text-xl tracking-wide ">Today</span>
+                      <span className="text-gray-300 text-xs">
+                        {moment().format('MMMM Do')}, {currentTime}
+                      </span>
+                    </div>
+                  </>
+                ) : (
+                  <Date />
+                )}
               </div>
               <div className="flex flex-col gap-y-4 text-white">
-                <div className="flex mx-auto">
-                  <span className="font-thin tracking-wide text-8xl">{currentWeather?.main.temp.toFixed(0)}</span>
-                  <span className="font-thin tracking-tighter text-3xl">°C</span>
+                <div className={`flex justify-center ${!currentWeather && 'gap-x-4'}`}>
+                  {currentWeather ? (
+                    <>
+                      <span className="font-thin tracking-wide text-8xl">{currentWeather?.main.temp.toFixed(0)}</span>
+                      <span className="font-thin tracking-tighter text-3xl">°C</span>
+                    </>
+                  ) : (
+                    <Temperature />
+                  )}
                 </div>
-                <span className="flex items-center gap-x-0.5 mx-auto text-gray-500 text-sm">
-                  {currentWeather?.name}, {currentWeather?.sys.country}
-                  <span>
-                    <MdOutlineOpenInNew />
-                  </span>
-                </span>
-                <span className="mx-auto text-gray-500">
-                  Feels like {currentWeather?.main.feels_like.toFixed(0)} <span className="text-white mx-3">•</span>{' '}
-                  Sunset {moment.unix(dailyForecast?.sunset).format('LT')}
-                </span>
+                {currentWeather ? (
+                  <>
+                    <span className="flex items-center gap-x-0.5 mx-auto text-gray-500 text-sm">
+                      {currentWeather?.name}
+                      {currentWeather && `, ${currentWeather?.sys.country}`}
+                      <span>
+                        <MdOutlineOpenInNew />
+                      </span>
+                    </span>
+                    <span className="mx-auto text-gray-500">
+                      Feels like {currentWeather?.main.feels_like.toFixed(0)} <span className="text-white mx-3">•</span>{' '}
+                      Sunset {moment.unix(dailyForecast?.sunset).format('LT')}
+                    </span>
+                  </>
+                ) : (
+                  <Location />
+                )}
               </div>
             </div>
           </div>
--- src/components/RainChart.tsx
+++ src/components/RainChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useSelector } from '../store';
 
 interface IIsActive {
   isActive: boolean;
@@ -10,7 +10,7 @@ function Chart({ isActive, percent }: IIsActive) {
     <div className="relative">
       <div
         className={`absolute bottom-0 ${isActive ? 'bg-yellow-400' : 'bg-gray-700'} w-[10px] rounded z-10`}
-        style={{ height: percent }}
+        style={{ height: percent && percent }}
       />
       <div className="absolute bottom-0 left-[0.9px] transform translate-x-1/2 z-0 border-l-gray-700 border-dashed border-l-2 w-1 h-full" />
     </div>
@@ -18,6 +18,8 @@ function Chart({ isActive, percent }: IIsActive) {
 }
 
 export default function RainChart() {
+  const items = useSelector((state) => state.weather?.forecasts?.hourly);
+
   return (
     <div className="flex flex-col gap-y-6">
       <h1 className="text-white">Chance Of Rain</h1>
@@ -28,12 +30,12 @@ export default function RainChart() {
             <span>rainy</span>
             <span>sunny</span>
           </div>
-          <Chart isActive={false} percent="21%" />
-          <Chart isActive={false} percent="19%" />
-          <Chart isActive={false} percent="49%" />
-          <Chart isActive percent="70%" />
-          <Chart isActive={false} percent="29%" />
-          <Chart isActive={false} percent="25%" />
+          <Chart isActive={false} percent={`${items && items[0].pop * 100}%`} />
+          <Chart isActive={false} percent={`${items && items[3].pop * 100}%`} />
+          <Chart isActive={false} percent={`${items && items[6].pop * 100}%`} />
+          <Chart isActive percent={`${items && items[9].pop * 100}%`} />
+          <Chart isActive={false} percent={`${items && items[12].pop * 100}%`} />
+          <Chart isActive={false} percent={`${items && items[15].pop * 100}%`} />
         </div>
       </div>
     </div>
